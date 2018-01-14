b3b1f1076209ae34d8c595b6867e104de2566067
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-14 08:15:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
-function addElementToBeginningOfArray(element, originalArray){
+function addElementToBeginningOfArray(originalArray, element){
   var newArray = [element, ...originalArray];
     return newArray;
 }
+
+addElementToBeginningOfArray([1], 'f', 'o', 'o')
 
 function destructivelyAddElementToBeginningOfArray(element, originalArray){
     
